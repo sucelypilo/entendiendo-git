1c1af8e07e0e8a1eea58fd14c3bce278f9865d72
Iris <sucely.pilo@ufm.edu>
2026-05-03 10:19:39 -0600

    acabo de poner otra linea

--- ruby.rb
+++ ruby.rb
@@ -16,3 +16,4 @@ end
 
 print "Mi nuevo tema en vscode es lila y me encanta!"
 print "Solo no me gusta mucho el color de en la terminal, pero es un detalle menor."
+print "En general, estoy muy satisfecho con mi nuevo tema y lo recomendaría a otros que busquen un cambio de apariencia en su editor de código."
