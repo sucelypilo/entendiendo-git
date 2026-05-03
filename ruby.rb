887e8ec9b251a5906fc11298fc8777a2afe4d22d
Iris <sucely.pilo@ufm.edu>
2026-05-03 09:27:24 -0600

    estoy agregando un archivo ruby en dev

---
+++ ruby.rb
@@ -0,0 +1,16 @@
+# Definir variables
+nombre = "Usuario"
+hora = 14
+
+# Condicional simple
+if hora < 12
+  puts "Buenos días, #{nombre}!"
+else
+  puts "Buenas tardes, #{nombre}!"
+end
+
+# Bucle simple
+3.times do
+  puts "¡Hola!"
+end
+
